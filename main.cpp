ac0d9aeaa05f4be0c71bc705c42718ba86942acd
Marius Egeland <mariue16@student.uia.no>
2018-09-26 13:51:43 +0200

    Revert "Changed main and main.cpp"
    This reverts commit 5f9318acb8fc4b611d9e16a929fa170b8544152d.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ int main()
 {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Dose this work ???" << std::endl;
-	std::cout << "I want to revert away this change" << std::endl;
 
 	return 0;
 }
