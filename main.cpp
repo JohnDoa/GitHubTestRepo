5f9318acb8fc4b611d9e16a929fa170b8544152d
Marius Egeland <mariue16@student.uia.no>
2018-09-26 13:41:07 +0200

    Changed main and main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Dose this work ???" << std::endl;
+	std::cout << "I want to revert away this change" << std::endl;
 
 	return 0;
 }
